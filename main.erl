3e89545ed74dec14879bfc65b75523113c7a3614
mat <matthew.kelcey@gmail.com>
2009-04-24 23:13:15 +1000

    do similarity calc in own process which own in process movie data dets

--- main.erl
+++ main.erl
@@ -2,15 +2,15 @@
 -compile(export_all).
 
 write_ratings() ->
-    movie_data:start(),
+    similarity:start(),
     Ids = movie_data:read_movie_ids(),
     io:format("#movies = ~w\n",[length(Ids)]),
     util:time("write ratings", fun() -> similarity:write_movie_ratings(Ids) end),    
-    movie_data:stop(),
+    similarity:stop(),
     init:stop().
 
 calc(Args) ->
-    movie_data:start(),
+    similarity:start(),
     Ids = movie_data:read_movie_ids(), 
     CalcFun = fun() -> 
 		      lists:foreach(
@@ -18,18 +18,18 @@ calc(Args) ->
 			parse_args(Args))
 		      end,
     util:time("calc similarity", CalcFun),
-    movie_data:stop(),
+    similarity:stop(),
     init:stop().
 
 checksum(Args) ->
-    movie_data:start(),
+    similarity:start(),
     Ids = movie_data:read_movie_ids(),
     lists:foreach(
       fun(Id) ->
 	      io:format("check ~w ~w\n",[Id, lists:sum(similarity:calc_all_for(Id,Ids))])
       end,
       parse_args(Args)),
-    movie_data:stop(),
+    similarity:stop(),
     init:stop().
 
 parse_args(Args) ->
--- similarity.erl
+++ similarity.erl
@@ -1,24 +1,53 @@
 -module(similarity).
--export([start/0, stop/0, write_movie_ratings/1, calc_for/2, calc_all_for/2]).
+-export([start/0, stop/0, write_movie_ratings/1, calc_for/2, calc_all_for/2, worker/0]).
+
+% api
 
 start() ->
-    ok.
+    register(worker, spawn(?MODULE, worker, [])),
+    worker ! start.
 
 stop() ->
-    ok.
+    worker ! stop.
 
 write_movie_ratings(Ids) ->
-    movie_data:write_movie_ratings(Ids).
+    worker ! { write_movie_ratings, Ids }.
+	     
+calc_all_for(Mid, Ids) ->	        
+    worker ! { calc_all_for, Mid, Ids, self() },
+    receive Coeffs -> Coeffs after 60000 -> timeout end.
 
-calc_for(M1,M2) ->
+% worker
+
+worker() ->
+    receive
+	start ->
+	    io:format("~w start\n",[self()]),
+	    movie_data:start(),
+	    worker();
+	stop ->
+	    io:format("~w stop\n",[self()]),
+	    movie_data:stop(),	    
+	    worker();
+	{write_movie_ratings, Ids} ->
+	    movie_data:write_movie_ratings(Ids),
+	    worker();
+	{calc_all_for, Mid, Ids, Pid} ->	
+	    io:format("~w calc_all_for ~w\n",[self(),Mid]),
+	    Coeffs = [ calc_for(Mid,OtherId) || OtherId <- Ids ],	    
+	    Pid ! Coeffs,
+	    worker()
+
+    after 10000 ->
+	    io:format("~w timeout\n",[self()])
+    end.
+
+calc_for(M1,M2) -> 
     Ratings1 = movie_data:ratings(M1),
     Ratings2 = movie_data:ratings(M2),
     {Sums, NumCommon} = coeff:summations(Ratings1,Ratings2),
     coeff:coeff(Sums,NumCommon).
     
-calc_all_for(Mid, Ids) ->	        
-    [ calc_for(Mid,OtherId) || OtherId <- Ids ].
-
 
 	    
 
